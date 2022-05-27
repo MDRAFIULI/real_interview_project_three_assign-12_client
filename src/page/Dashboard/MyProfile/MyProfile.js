df74190bd6062eeaff59e8728c88516a6238c52e
RafiUlI <ctgrafi150@gmail.com>
2022-05-27 10:11:01 +0600

    Profile start

--- src/page/Dashboard/MyProfile/MyProfile.js
+++ src/page/Dashboard/MyProfile/MyProfile.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 const MyProfile = () => {
+    const [user] = useAuthState(auth);
+    console.log(user);
     return (
-        <div className='mx-auto'>
-            <h1>MyProfile</h1>
+        <div>
+            <div class="card w-96 bg-black shadow-xl mt-8">
+                <div class="avatar mx-auto my-8">
+                    <div class="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                        <img src={user?.photoURL} />
+                    </div>
+                </div>
+                <div class="card-body">
+                    <h2 class=" text-3xl text-center text-white">My Profile</h2>
+                    <h1 className='text-center text-white'>{user?.displayName}</h1>
+                    <h1 className='text-center text-white'>{user?.email}</h1>
+                </div>
+            </div>
         </div>
     );
 };
--- src/page/Home/Products/Products.js
+++ src/page/Home/Products/Products.js
@@ -17,7 +17,7 @@ const Products = () => {
             <h1 className="text-4xl text-center text-[#0AA1DD]">Tools Of Heaven Brand!</h1>
             <div className='grid lg:grid-cols-2 gap-24 mt-16 container ml-8'>
                 {
-                    products.slice(0, 6).map(product => <Product key={product._id} product={product}></Product>)
+                    products?.slice(0, 6).map(product => <Product key={product._id} product={product}></Product>)
                 }
             </div>
         </div>
